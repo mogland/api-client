13317eb0e6f9f2a976f866bc604056caa509580b
wibus-wee <1596355173@qq.com>
2023-02-12 15:07:38 +0800

    feat(adapters): ofetch adapter

---
+++ src/adapters/ofetch.ts
@@ -0,0 +1,42 @@
+import { ofetch } from "ofetch";
+import { IAdapter, IAdapterOptions, IAdapterResponse } from ".";
+
+const $ = ofetch.create({});
+
+export const ofetchAdapter: IAdapter<typeof ofetch> = Object.preventExtensions({
+  get default() {
+    return $;
+  },
+
+  get(url, options) {
+    return $(url, options);
+  },
+
+  post(url, options) {
+    return $(url, {
+      ...options,
+      method: "POST",
+    });
+  },
+
+  put(url, options) {
+    return $(url, {
+      ...options,
+      method: "PUT",
+    });
+  },
+
+  patch(url, options) {
+    return $(url, {
+      ...options,
+      method: "PATCH",
+    });
+  },
+
+  delete(url, options) {
+    return $(url, {
+      ...options,
+      method: "DELETE",
+    });
+  },
+});
